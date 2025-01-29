f3d30f20887a3c3189f6d4f2526cb9a61021edbc
Alejandro <alevalencigiraldo@gmail.com>
2025-01-29 20:36:19 +0100

    Create imagesGrid to HeroSection

--- src/components/home_components/HeroSection.jsx
+++ src/components/home_components/HeroSection.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ImageGrid } from "../ImageGrid.jsx";
 
 export const HeroSection = ({ title, name, subtitle, description, links }) => {
     return (
         <section className="h-screen flex flex-col items-center my-10 px-6">
-            <h1 className="text-4xl md:text-6xl font-bold text-center mb-4">
-                {title} <span className="text-teal-400">{name}</span>!
+            <h1 className="text-4xl md:text-6xl font-bold text-center mb-10">
+                {title}! <span className="text-teal-400">{name}</span>
             </h1>
             <h2 className="text-xl md:text-2xl text-center mb-6">
                 {subtitle}
@@ -18,19 +19,20 @@ export const HeroSection = ({ title, name, subtitle, description, links }) => {
                     <Link
                         key={index}
                         to={link.to}
-                        className="bg-teal-500 hover:bg-teal-600 text-white px-6 py-3 rounded-lg transition" // clase CSS genérica
+                        className="bg-teal-500 hover:bg-teal-600 text-white px-6 py-3 rounded-lg transition"
                     >
                         {link.text}
                     </Link>
                 ))}
             </div>
-            <div className="mt-10">
-                <img
-                    src="/path/to/your/image.png"
-                    alt="Representación de desarrollo web"
-                    className="w-72 md:w-96 mx-auto"
-                />
-            </div>
+            <ImageGrid
+                images={[
+                    { url: 'https://winatalent.com/blog/wp-content/uploads/2023/12/Best-Programming-Software-for-Writing-Code.jpg', alt: 'Picture 1' },
+                    { url: 'https://winatalent.com/blog/wp-content/uploads/2023/12/Best-Programming-Software-for-Writing-Code.jpg', alt: 'Picture 2' },
+                    { url: 'https://winatalent.com/blog/wp-content/uploads/2023/12/Best-Programming-Software-for-Writing-Code.jpg', alt: 'Picture 3' },
+                ]}
+                columns={3}
+            />
         </section>
     );
 }

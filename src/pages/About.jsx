d7879fc1aaf8b330e3b5edcffec9f1b75e012634
Alejandro <alevalencigiraldo@gmail.com>
2025-04-20 12:27:20 +0200

    fixing footer and some stuffs..

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div className="flex flex-col my-10 h-screen">
-            <div className="my-10">
-                <h1 className="text-3xl text-center font-bold my-10">Sobre mi</h1>
-                <p className="text-center max-w-2xl text-gray-300 mb-8">
-                    Soy un desarrollador Full Stack con experiencia en desarrollo web y móvil.
-                    Actualmente trabajo en el desarrollo de aplicaciones web y móviles para una empresa de tecnología.
-                </p>
-            </div>
+        <div className="flex flex-col items-center text-center my-10 h-screen">
+            <h1 className="text-3xl font-bold my-10">Sobre mi</h1>
+            <p className="max-w-2xl text-gray-300 mb-8">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, ex impedit nisi, ratione,
+                quis ipsum beatae corporis tempore deserunt fugiat nostrum repellat eaque deleniti iure itaque praesentium architecto facilis asperiores?
+            </p>
         </div>
+
     )
 }
